f70f5d219a7e454e8ca7ba06b057f5205a56718e
raf256 <1rafaelmodesto@gmail.com>
2023-02-28 18:48:40 -0500

    chatbot setup

--- hello_world.py
+++ hello_world.py
@@ -3,14 +3,18 @@ import openai
 
 def open_file(filepath):
     with open(filepath, 'r', encoding='utf-8') as infile:
-        return infile.read()
+        return infile.read().strip()
 
 
 openai.api_key = open_file('openaiapikey.txt')
 
 
 def gpt3_completion(prompt, engine='text-davinci-002', temp=0.7, top_p=1.0, tokens=400, freq_pen=0.0, pres_pen=0.0, stop=['<<END>>']):
-    prompt = prompt.encode(encoding='ASCII',errors='ignore').decode()
+    prompt = prompt.encode(encoding='ASCII', errors='ignore').decode()
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': f'Bearer {openai.api_key}',
+    }
     response = openai.Completion.create(
         engine=engine,
         prompt=prompt,
@@ -19,7 +23,9 @@ def gpt3_completion(prompt, engine='text-davinci-002', temp=0.7, top_p=1.0, toke
         top_p=top_p,
         frequency_penalty=freq_pen,
         presence_penalty=pres_pen,
-        stop=stop)
+        stop=stop,
+        headers=headers,
+    )
     text = response['choices'][0]['text'].strip()
     return text
 
